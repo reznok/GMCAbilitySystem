d31b6b86a00731184dbf61f0e2eb0096dc8959ee
Peter Gilbert <petegilb@bu.edu>
2024-03-07 00:09:32 -0800

    remove merge conflict message

--- Source/GMCAbilitySystem/Private/Ability/Tasks/WaitForInputKeyRelease.cpp
+++ Source/GMCAbilitySystem/Private/Ability/Tasks/WaitForInputKeyRelease.cpp
@@ -14,11 +14,7 @@ void UGMCAbilityTask_WaitForInputKeyRelease::Activate()
 {
 	Super::Activate();
 	
-<<<<<<< HEAD
 	if (Ability->AbilityInputAction != nullptr)
-=======
-	if (Ability->AbilityInputAction  != nullptr)
->>>>>>> 278dcad... tagtargetdata task, parent tag helper function, update input action var on ability
 	{
 		InputActionBinding = &GetEnhancedInputComponent()->BindActionValue(Ability->AbilityInputAction);
 	}
