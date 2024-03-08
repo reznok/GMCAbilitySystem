74a05b5fc8969c51cd4fa957665d88c0e4a44371
Brandon Forbes <reznok@gmail.com>
2024-03-08 13:24:14 -0800

    Add TargetData Int task

---
+++ Source/GMCAbilitySystem/Private/Ability/Tasks/SetTargetDataInt.cpp
@@ -0,0 +1,44 @@
+﻿#include "Ability/Tasks/SetTargetDataInt.h"
+
+#include "GMCAbilityComponent.h"
+
+
+UGMCAbilityTask_SetTargetDataInt* UGMCAbilityTask_SetTargetDataInt::SetTargetDataInt(UGMCAbility* OwningAbility,
+                                                                                                 int Int)
+{
+	UGMCAbilityTask_SetTargetDataInt* Task = NewAbilityTask<UGMCAbilityTask_SetTargetDataInt>(OwningAbility);
+	Task->Ability = OwningAbility;
+	Task->Target = Int;
+	return Task;
+}
+
+void UGMCAbilityTask_SetTargetDataInt::Activate()
+{
+	Super::Activate();
+
+	if (AbilitySystemComponent->GetNetMode() != NM_DedicatedServer)
+	{
+		ClientProgressTask();
+	}
+}
+
+void UGMCAbilityTask_SetTargetDataInt::ProgressTask(FInstancedStruct& TaskData)
+{
+	Super::ProgressTask(TaskData);
+	const FGMCAbilityTaskTargetDataInt Data = TaskData.Get<FGMCAbilityTaskTargetDataInt>();
+	
+	Completed.Broadcast(Data.Target);
+	EndTask();
+}
+
+void UGMCAbilityTask_SetTargetDataInt::ClientProgressTask()
+{
+	FGMCAbilityTaskTargetDataInt TaskData;
+	TaskData.TaskType = EGMCAbilityTaskDataType::Progress;
+	TaskData.AbilityID = Ability->GetAbilityID();
+	TaskData.TaskID = TaskID;
+	TaskData.Target = Target;
+	const FInstancedStruct TaskDataInstance = FInstancedStruct::Make(TaskData);
+	
+	Ability->OwnerAbilityComponent->QueueTaskData(TaskDataInstance);
+}
---
+++ Source/GMCAbilitySystem/Public/Ability/Tasks/SetTargetDataInt.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "GMCAbilityTaskBase.h"
+#include "GMCAbilityTaskData.h"
+#include "Ability/GMCAbility.h"
+#include "LatentActions.h"
+#include "Engine/CancellableAsyncAction.h"
+#include "SetTargetDataInt.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FUGMCAbilityTaskTargetDataIntAsyncActionPin, int, Target);
+
+USTRUCT(BlueprintType)
+struct FGMCAbilityTaskTargetDataInt : public FGMCAbilityTaskData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadWrite)
+	int Target{0};
+};
+
+UCLASS()
+class UGMCAbilityTask_SetTargetDataInt : public UGMCAbilityTaskBase
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintAssignable)
+	FUGMCAbilityTaskTargetDataIntAsyncActionPin	Completed;
+	
+	UPROPERTY(BlueprintReadWrite)
+	int Target;
+	
+	virtual void ProgressTask(FInstancedStruct& TaskData) override;
+	virtual void ClientProgressTask() override;
+ 
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "TRUE"), DisplayName="Set Target Data (Int)",Category = "GMCAbilitySystem/Tasks")
+	static UGMCAbilityTask_SetTargetDataInt* SetTargetDataInt(UGMCAbility* OwningAbility, int Int);
+ 
+	//Overriding BP async action base
+	virtual void Activate() override;
+};
