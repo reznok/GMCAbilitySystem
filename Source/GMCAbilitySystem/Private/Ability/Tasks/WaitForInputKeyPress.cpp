5406c31ec215ccbfad19de805e63ebdf872a882b
Eric Rajot <aherys@deepworlds.studio>
2024-05-09 22:05:25 +0200

    Adding Two Ability Task - Adding Target Data Object - Adding Wait For Input Key Press

---
+++ Source/GMCAbilitySystem/Private/Ability/Tasks/WaitForInputKeyPress.cpp
@@ -0,0 +1,100 @@
+﻿// © Deep Worlds SA
+#include "Ability/Tasks/WaitForInputKeyPress.h"
+
+#include "EnhancedInputComponent.h"
+#include "SNegativeActionButton.h"
+#include "Components/GMCAbilityComponent.h"
+
+
+UGMCAbilityTask_WaitForInputKeyPress* UGMCAbilityTask_WaitForInputKeyPress::WaitForKeyPress(UGMCAbility* OwningAbility)
+{
+	UGMCAbilityTask_WaitForInputKeyPress* Task = NewAbilityTask<UGMCAbilityTask_WaitForInputKeyPress>(OwningAbility);
+	Task->Ability = OwningAbility;
+	return Task;
+}
+
+void UGMCAbilityTask_WaitForInputKeyPress::Activate()
+{
+	Super::Activate();
+
+	if (Ability->bAllowMultipleInstances) {
+		UE_LOG(LogGMCAbilitySystem, Warning, TEXT("Ability %s is set to allow multiple instances and this should not be used with WaitForInputKeyPress AbilityTask !"), *Ability->GetName());
+		ClientProgressTask();
+		return;
+	}
+	
+	if (Ability->AbilityInputAction != nullptr)
+	{
+		InputActionBinding = &GetEnhancedInputComponent()->BindActionValue(Ability->AbilityInputAction);
+	}
+	else
+	{
+		ClientProgressTask();
+	}
+}
+
+UEnhancedInputComponent* UGMCAbilityTask_WaitForInputKeyPress::GetEnhancedInputComponent() const
+{
+	UInputComponent* InputComponent = Ability->OwnerAbilityComponent->GetOwner()->GetComponentByClass<UInputComponent>();
+	if (InputComponent)
+	{
+		if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+		{
+			return EnhancedInputComponent;
+		}
+	}
+	return nullptr;
+}
+
+void UGMCAbilityTask_WaitForInputKeyPress::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (bTaskCompleted) {
+		return;
+	}
+	
+	if (AbilitySystemComponent->GetNetMode() == NM_DedicatedServer)
+	{
+		return;
+	}
+	
+	
+	if(InputActionBinding == nullptr || (InputActionBinding->GetValue().Get<bool>() && bFirstReleaseTriggerPass))
+	{
+		ClientProgressTask();
+	}
+	else if (!bFirstReleaseTriggerPass && !InputActionBinding->GetValue().Get<bool>())
+	{
+		bFirstReleaseTriggerPass = true;
+	}
+}
+
+void UGMCAbilityTask_WaitForInputKeyPress::OnTaskCompleted()
+{
+	EndTask();
+	Completed.Broadcast();
+	bTaskCompleted = true;
+}
+
+void UGMCAbilityTask_WaitForInputKeyPress::OnDestroy(bool bInOwnerFinished)
+{
+	Super::OnDestroy(bInOwnerFinished);
+}
+
+void UGMCAbilityTask_WaitForInputKeyPress::ProgressTask(FInstancedStruct& TaskData)
+{
+	Super::ProgressTask(TaskData);
+	OnTaskCompleted();
+}
+
+void UGMCAbilityTask_WaitForInputKeyPress::ClientProgressTask()
+{
+	FGMCAbilityTaskData TaskData;
+	TaskData.TaskType = EGMCAbilityTaskDataType::Progress;
+	TaskData.AbilityID = Ability->GetAbilityID();
+	TaskData.TaskID = TaskID;
+	const FInstancedStruct TaskDataInstance = FInstancedStruct::Make(TaskData);
+	
+	Ability->OwnerAbilityComponent->QueueTaskData(TaskDataInstance);
+}
+
---
+++ Source/GMCAbilitySystem/Public/Ability/Tasks/WaitForInputKeyPress.h
@@ -0,0 +1,48 @@
+﻿// © Deep Worlds SA
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ability/Tasks/GMCAbilityTaskBase.h"
+#include "EnhancedInputComponent.h"
+#include "Ability/GMCAbility.h"
+#include "InstancedStruct.h"
+#include "LatentActions.h"
+#include "WaitForInputKeyPress.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FAbilityTaskWaitForInputKeyPress);
+
+/**
+ * 
+ */
+UCLASS()
+class GMCABILITYSYSTEM_API UGMCAbilityTask_WaitForInputKeyPress : public UGMCAbilityTaskBase {
+	GENERATED_BODY()
+
+	public:
+
+		UFUNCTION()
+		virtual void Tick(float DeltaTime) override;
+
+		void OnTaskCompleted();
+		virtual void OnDestroy(bool bInOwnerFinished) override;
+
+		virtual void ProgressTask(FInstancedStruct& TaskData) override;
+		virtual void ClientProgressTask() override;
+		 
+		UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "TRUE"), DisplayName="Wait For Input Key Press",Category = "GMCAbilitySystem/Tasks")
+		static UGMCAbilityTask_WaitForInputKeyPress* WaitForKeyPress(UGMCAbility* OwningAbility);
+	 
+		//Overriding BP async action base
+		virtual void Activate() override;
+
+		UPROPERTY(BlueprintAssignable)
+		FAbilityTaskWaitForInputKeyPress Completed;
+
+	private:
+		FEnhancedInputActionValueBinding* InputActionBinding;
+		UEnhancedInputComponent* GetEnhancedInputComponent() const;
+
+		// We need to release in first the key, to be able to cut it by the same key, so we store the first input here.
+		bool bFirstReleaseTriggerPass = false;
+};
