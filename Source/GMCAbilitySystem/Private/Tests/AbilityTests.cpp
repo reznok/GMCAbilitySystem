5e25611413aa21267924f7901ee9ccd12c150707
Harrison Lambeth <harrison@nau.edu>
2024-07-03 21:36:16 -0700

    some basic unit tests for CanAffordAbilityCost()

---
+++ Source/GMCAbilitySystem/Private/Tests/AbilityTests.cpp
@@ -0,0 +1,86 @@
+﻿#include "Attributes/GMCAttributesData.h"
+#include "Ability/GMCAbility.h"
+#include "Fixtures/UAbilityCostEffect.h"
+#include "Misc/AutomationTest.h"
+
+BEGIN_DEFINE_SPEC(AbilityTestsSpec, "GMCAbilitySystem.AbilityTestsSpec", EAutomationTestFlags::ProductFilter | EAutomationTestFlags::ApplicationContextMask)
+	UGMC_AbilitySystemComponent* AbilitySystemComponent;
+	TMap<FName, FGameplayTag> TagMap;
+END_DEFINE_SPEC(AbilityTestsSpec)
+void AbilityTestsSpec::Define()
+{
+	Describe("CanAffordAbilityCost()", [this]()
+	{
+		BeforeEach([this]()
+		{
+			TagMap = CreateTagMap();
+			
+			// Attributes
+			UGMCAttributesData* AttributesData = NewObject<UGMCAttributesData>();
+			AttributesData->AttributeData.Add({
+				TagMap[FName("Attribute.Points")],
+				0.f,
+				FAttributeClamp(),
+				true,
+			});
+
+			// GMCMovementComponent
+			UGMC_MovementUtilityCmp* MovementComponent = NewObject<UGMC_MovementUtilityCmp>();
+			
+			// Ability System Component
+			AbilitySystemComponent = NewObject<UGMC_AbilitySystemComponent>();
+			AbilitySystemComponent->GMCMovementComponent = MovementComponent;
+			AbilitySystemComponent->AttributeDataAssets.Add(AttributesData);
+
+			AbilitySystemComponent->BindReplicationData();
+		});
+		
+		It("should return true if there is no cost defined", [this]()
+		{
+			// Arrange
+			UGMCAbility* Ability = NewObject<UGMCAbility>();
+			Ability->OwnerAbilityComponent = AbilitySystemComponent;
+			Ability->AbilityCost = nullptr;
+
+			// Act
+			const bool bCanAfford = Ability->CanAffordAbilityCost();
+
+			// Assert
+			TestTrue("Can afford ability cost", bCanAfford);
+		});
+
+		It("should return false if the cost cannot be afforded", [this]()
+		{	
+			// Arrange
+			
+			UGMCAbility* Ability = NewObject<UGMCAbility>();
+			Ability->OwnerAbilityComponent = AbilitySystemComponent;
+			Ability->AbilityCost = UAbilityCostEffect::StaticClass();
+
+			// Act
+			const bool bCanAfford = Ability->CanAffordAbilityCost();
+
+			// Assert
+			TestFalse("Cannot afford ability cost", bCanAfford);
+		});
+
+		It("should return true if the cost can be afforded", [this]()
+		{	
+			// Arrange
+			UGMCAbility* Ability = NewObject<UGMCAbility>();
+			Ability->OwnerAbilityComponent = AbilitySystemComponent;
+			Ability->AbilityCost = UAbilityCostEffect::StaticClass();
+			AbilitySystemComponent->ApplyAbilityEffectModifier({
+				TagMap[FName("Attribute.Points")],
+				20.f,
+				EModifierType::Add,
+			}, true, false);
+
+			// Act
+			const bool bCanAfford = Ability->CanAffordAbilityCost();
+
+			// Assert
+			TestTrue("Can afford ability cost", bCanAfford);
+		});
+	});
+}
---
+++ Source/GMCAbilitySystem/Private/Tests/Fixtures/CreateTagMap.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "KismetCastingUtils.h"
+#include "Effects/GMCAbilityEffect.h"
+#include "UObject/Object.h"
+
+inline TMap<FName, FGameplayTag> CreateTagMap()
+{
+	TMap<FName, FGameplayTag> TagMap;
+	
+	FGameplayTagContainer Container;
+	UGameplayTagsManager::Get().RequestAllGameplayTags(Container, true);
+	TArray<FGameplayTag> TagArray = Container.GetGameplayTagArray();
+
+	TagArray.Sort();
+
+	TagMap.Add(FName("Attribute.Points"), TagArray[0]);
+	TagMap.Add(FName("Effect.AbilityCost"), TagArray[1]);
+
+	return TagMap;
+}
