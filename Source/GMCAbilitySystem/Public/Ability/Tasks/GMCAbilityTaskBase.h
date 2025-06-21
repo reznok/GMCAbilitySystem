2d3f590ccefbc12674872cba8dd63f9daec86859
Aherys <aherys@deepworlds.studio>
2025-06-21 03:13:40 +0200

    Higher task heartbeat value.

--- Source/GMCAbilitySystem/Public/Ability/Tasks/GMCAbilityTaskBase.h
+++ Source/GMCAbilitySystem/Public/Ability/Tasks/GMCAbilityTaskBase.h
@@ -79,14 +79,14 @@ protected:
 
 private:
 	// How often client sends heartbeats to server
-	float HeartbeatInterval = .1f;
+	float HeartbeatInterval = .2f;
 
 	// Max time between heartbeats before server cancels task
 	// Aherys: previous value was 0.3f it's maybe a bit too low for harsh network conditions
-	float HeartbeatMaxInterval =1.5f;
+	float HeartbeatMaxInterval = 3.f;
 	
-	float ClientLastHeartbeatSentTime;
-	float LastHeartbeatReceivedTime;
+	float ClientLastHeartbeatSentTime = 0.f;
+	float LastHeartbeatReceivedTime = 0.f;
 
 
 };
