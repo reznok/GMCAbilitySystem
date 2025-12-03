a8900aec04f2f38151c4df9bbf22d7ffbdfb6fcf
Aherys <aherys@deepworlds.studio>
2025-12-03 17:11:18 +0100

    Removed confusing and useless log.

--- Source/GMCAbilitySystem/Private/Components/GMCAbilityComponent.cpp
+++ Source/GMCAbilitySystem/Private/Components/GMCAbilityComponent.cpp
@@ -1101,10 +1101,6 @@ void UGMC_AbilitySystemComponent::RPCTaskHeartbeat_Implementation(int AbilityID,
 	{
 		ActiveAbilities[AbilityID]->HandleTaskHeartbeat(TaskID);
 	}
-	else
-	{
-		UE_LOG(LogGMCAbilitySystem, Error, TEXT("[RPC] Server Heartbeat for non-existent Ability: %d"), AbilityID);
-	}
 }
 
 void UGMC_AbilitySystemComponent::RPCClientEndAbility_Implementation(int AbilityID)
