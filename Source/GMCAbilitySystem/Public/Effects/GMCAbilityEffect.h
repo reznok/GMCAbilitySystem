984c45f7219e581d4d6ca12eadc48c98a3f49fc5
Aherys <aherys@deepworlds.studio>
2026-01-05 04:08:36 +0100

    Refactor editor-only directives for better consistency
    Moved editor-only logic from `WITH_EDITORONLY_DATA` to `WITH_EDITOR` across multiple files for improved clarity and alignment with Unreal Engine guidelines. Also updated includes and properties to ensure proper functionality within the editor environment.

--- Source/GMCAbilitySystem/Public/Effects/GMCAbilityEffect.h
+++ Source/GMCAbilitySystem/Public/Effects/GMCAbilityEffect.h
@@ -7,6 +7,7 @@
 #include "UObject/Object.h"
 #include "GMCAbilitySystem.h"
 #include "GMCAttributeModifier.h"
+#include "Attributes/GMCAttributeModifierCustom_Base.h"
 #include "GMCAbilityEffect.generated.h"
 
 class UGMC_AbilitySystemComponent;
